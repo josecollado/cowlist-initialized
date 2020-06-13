5f9ea79a5f8186c3a13562ae056961c62083ce1d
Jose Collado <jcoll1020@gmail.com>
2020-06-12 20:13:28 -0400

    Server is up and running, DB is connected, now working on frontend

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,62 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      descView: false,
+      showCow: '',
+      showDesc: '',
+      cows: {
+        Buttercup: '	a herbaceous plant with bright yellow cup-shaped flowers, common in grassland and as a garden weed. All kinds are poisonous and generally avoided by livestock.',
+        Daisy: '	a small grassland plant that has flowers with a yellow disk and white rays. It has given rise to many ornamental garden varieties.',
+        Milkshake:'a cold drink made of milk, a sweet flavoring such as fruit or chocolate, and typically ice cream, whisked until it is frothy.',
+        Bessie: 'a person\'s best or closest friend.',
+        MooDonna: '	archaic : lady -- used as a form of respectful address.',
+        MooLawn: 'a legendary Chinese warrior from the Northern and Southern dynasties period (420–589) of Chinese history.'
+      }
+
+    }
+  }
+
+
+  handleClick(e){
+    let name = e.target.innerHTML
+    this.setState({
+      descView: !false,
+      showCow: name,
+      showDesc: this.state.cows[name]
+    })
+  }
   render() {
     return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Cow-List</h1>
+      <div>
+        <ol>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>Buttercup</p>
+          </li>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>Daisy</p>
+          </li>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>Milkshake</p>
+          </li>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>Bessie</p>
+          </li>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>MooDonna</p>
+          </li>
+          <li>
+            <p onClick={this.handleClick.bind(this)}>MooLawn</p>
+          </li>
+        </ol>
+      </div>
+      <div>
+      </div>
+    </div>  
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,46 @@
 const express = require('express')
+const mongoose =require('mongoose')
 const app = express()
 const port = 3000
+mongoose.connect('mongodb://localhost/cowlist')
+
+let cowSchema = mongoose.Schema({
+    _id: Number,
+    name: String,
+    description: String
+})
+
+let Cow = mongoose.model('cows', cowSchema)
 
 app.use(express.static('./client/dist'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.send('index').status(200)
+})
+
+
+app.get('/api/cows', (req, res) =>{
+    console.log('hello from cows get')
+    res.send('hi')
+})
+
+app.post('/api/cows' , (req,res) => {
+    console.log('hello from cows post')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
